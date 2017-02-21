3f74da0b3b78989a099b00555b5d76db91ff305d
SemenSizov <sizov.semen@gmail.com>
2017-02-21 11:30:57 +0200

    task 5 - edited.

--- 5.sql
+++ 5.sql
@@ -1,10 +1,10 @@
 SELECT
-  customers.custName,
+  customers.name,
   companies.name,
   projects.cost
 FROM customers
-INNER JOIN cust_project on customers.id = cust_project.customer_id
-INNER JOIN projects ON cust_project.project_id = projects.id
+INNER JOIN projects_customers on customers.id = projects_customers.customers_id
+INNER JOIN projects ON projects_customers.project_id = projects.id
 INNER JOIN project_company ON projects.id = project_company.project_id
 INNER JOIN companies ON project_company.company_id = companies.id
 WHERE cost=(SELECT id
