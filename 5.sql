f19ffe49056335f7904c3106ce0fd1c2a86ecb2f
Anastasiya Kravchenko <staska_1997@mail.ru>
2017-02-25 20:21:36 +0200

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -1,16 +1,22 @@
 SELECT
-  customers.name,
-  companies.name,
-  projects.cost
-FROM customers
-INNER JOIN projects_customers on customers.id = projects_customers.customers_id
-INNER JOIN projects ON projects_customers.project_id = projects.id
-INNER JOIN project_company ON projects.id = project_company.project_id
-INNER JOIN companies ON project_company.company_id = companies.id
-WHERE cost=(SELECT id
-  FROM projects
-  HAVING min(cost))
-GROUP BY companies.name;
+  customerName,
+  companyName,
+  MIN(projectCost)
+FROM
+  ((SELECT
+      projects.name      AS projectName,
+      SUM(projects.cost) AS projectCost,
+      customers.name     AS customerName,
+      companies.name     AS companyName
+    FROM projects
+      INNER JOIN projects_customers ON projects.id = projects_customers.project_id
+      INNER JOIN customers ON customers.id = projects_customers.customers_id
+      INNER JOIN project_company ON projects.id = project_company.project_id
+      INNER JOIN companies ON companies.id = project_company.company_id
+
+    GROUP BY companies.name, customers.name) AS Stat)
+
+GROUP BY companyName
 
 
 
